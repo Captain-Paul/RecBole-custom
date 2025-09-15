199241686c443cec169b75bd1e09ab6366c3d5c5
Shenghao Zhao <37969661+Captain-Paul@users.noreply.github.com>
2025-09-15 22:18:49 +0800

    substitute run_recbole

--- run_recbole.py
+++ run_recbole.py
@@ -3,53 +3,36 @@
 # @Email  : slmu@ruc.edu.cn
 
 # UPDATE
-# @Time   : 2022/7/8, 2020/10/3, 2020/10/1
-# @Author : Zhen Tian, Yupeng Hou, Zihan Lin
-# @Email  : chenyuwuxinn@gmail.com, houyupeng@ruc.edu.cn, zhlin@ruc.edu.cn
+# @Time   : 2020/10/3, 2020/10/1
+# @Author : Yupeng Hou, Zihan Lin
+# @Email  : houyupeng@ruc.edu.cn, zhlin@ruc.edu.cn
+
 
 import argparse
+import time
+from recbole.quick_start import run_recbole
 
-from recbole.quick_start import run
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    begin = time.time()
+    parameter_dict = {
+        'neg_sampling': None
+        # 'gpu_id':3,
+        # 'attribute_predictor':'not',
+        # 'attribute_hidden_size':"[256]",
+        # 'fusion_type':'gate',
+        # 'seed':212,
+        # 'n_layers':4,
+        # 'n_heads':1
+    }
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", "-m", type=str, default="BPR", help="name of models")
-    parser.add_argument(
-        "--dataset", "-d", type=str, default="ml-100k", help="name of datasets"
-    )
-    parser.add_argument("--config_files", type=str, default=None, help="config files")
-    parser.add_argument(
-        "--nproc", type=int, default=1, help="the number of process in this group"
-    )
-    parser.add_argument(
-        "--ip", type=str, default="localhost", help="the ip of master node"
-    )
-    parser.add_argument(
-        "--port", type=str, default="5678", help="the port of master node"
-    )
-    parser.add_argument(
-        "--world_size", type=int, default=-1, help="total number of jobs"
-    )
-    parser.add_argument(
-        "--group_offset",
-        type=int,
-        default=0,
-        help="the global rank offset of this group",
-    )
+    parser.add_argument('--model', '-m', type=str, default='SASRecD', help='name of models')
+    parser.add_argument('--dataset', '-d', type=str, default='mlRating', help='name of datasets')
+    parser.add_argument('--config_files', type=str, default='configs/mlRating.yaml', help='config files')
 
     args, _ = parser.parse_known_args()
 
-    config_file_list = (
-        args.config_files.strip().split(" ") if args.config_files else None
-    )
-
-    run(
-        args.model,
-        args.dataset,
-        config_file_list=config_file_list,
-        nproc=args.nproc,
-        world_size=args.world_size,
-        ip=args.ip,
-        port=args.port,
-        group_offset=args.group_offset,
-    )
+    config_file_list = args.config_files.strip().split(' ') if args.config_files else None
+    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list,config_dict=parameter_dict)
+    end=time.time()
+    print(end-begin)
